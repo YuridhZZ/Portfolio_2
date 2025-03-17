46b57de0241605be558284853454d777e1845e59
YuridhZZ <yuridhzahrawan@gmail.com>
2025-03-17 15:55:36 +0700

    Update AboutMe.js

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -5,7 +5,7 @@ const AboutMe = () => {
     <div className="flex flex-col md:flex-row pb-6">
       <div className="w-full md:w-1/2 text-zinc-400 px-6 border-r-[1px] border-r-zinc-800 flex items-center">
         <div className="py-6">
-          <h2 className="font-semibold mb-1">Hello! I'm Zulkarnain or you can call me Yuridh</h2>
+          <h2 className="font-semibold mb-1">Hello! I'm Yuridh</h2>
           <p className="text-base leading-6 ">
           As a recent graduate with a Bachelor's degree in Information Technology with a strong foundation in IT. I  am dedicated to continual learning and growth in the field of Information Technology. 
           With an inquisitive and analytical mindset, combined with a diligent approach to tasks, I am poised to contribute effectively to any team or project. 
